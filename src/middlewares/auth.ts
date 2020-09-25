adaa3533b10f54888b6a0ca80eff4cfd9e153081
Franco Casas <casassfranco@gmail.com>
2020-09-25 12:49:42 -0300

    Se agrego database a el .gitignore

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,47 +1,47 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 class AuthMiddleware {
 
-    // verifyUserExists(req: any, res: any, next: Function) {
-    //     let user = req.nameUser;
+  // verifyUserExists(req: any, res: any, next: Function) {
+  //     let user = req.nameUser;
 
-    //     // if ( role === 'ADMIN_ROLE' || role === 'SUPER_ROLE') {
-    //     //     next()
-    //     // } else {
-    //     //     return res.status(401).json({
-    //     //         ok: false,
-    //     //         err: { msg: 'User Unauthorized' }
-    //     //     });
-    //     // }
-    // }
-    
-    verifyToken(req: any, res: any, next: Function) {
-        let token = req.get('token');
-        const seed: string = process.env.SEED ? process.env.SEED : '';
-        jwt.verify( token, seed, (err: any, decoded: any) => {
-            if (err) { 
-                return res.status(401).json({ ok: false, err })
-            }
-            req.user = (<any>decoded).user; 
-            // console.log(req.user);
-            next();
-        }
-    )}
+  //     // if ( role === 'ADMIN_ROLE' || role === 'SUPER_ROLE') {
+  //     //     next()
+  //     // } else {
+  //     //     return res.status(401).json({
+  //     //         ok: false,
+  //     //         err: { msg: 'User Unauthorized' }
+  //     //     });
+  //     // }
+  // }
 
-    verifyAdminRole(req: any, res: any, next: Function) {
-        let user = req.user;
-        let role = user.descriptUserRole
-        if ( role === 'ADMIN_ROLE' || role === 'SUPER_ROLE') {
-            next()
-        } else {
-            return res.status(401).json({
-                ok: false,
-                err: { msg: 'User Unauthorized' }
-            });
-        }
+  verifyToken(req: any, res: any, next: Function) {
+    let token = req.get("token");
+    const seed: string = process.env.SEED ? process.env.SEED : "";
+    jwt.verify(token, seed, (err: any, decoded: any) => {
+      if (err) {
+        return res.status(401).json({ ok: false, err });
+      }
+      req.user = (<any>decoded).user;
+      // console.log(req.user);
+      next();
+    });
+  }
+
+  verifyAdminRole(req: any, res: any, next: Function) {
+    let user = req.user;
+    let role = user.descriptUserRole;
+    if (role === "ADMIN_ROLE" || role === "SUPER_ROLE") {
+      next();
+    } else {
+      return res.status(401).json({
+        ok: false,
+        err: { msg: "User Unauthorized" },
+      });
     }
-
+  }
+  
 }
 
 const authMiddleware = new AuthMiddleware();
-export default authMiddleware; 
+export default authMiddleware;
